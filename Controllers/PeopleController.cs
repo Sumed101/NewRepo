17597daa23dff081ab607eca047e25a011e4e42b
aleka <aleka@SUMEDSVIVOBOOK>
2025-02-27 22:36:56 +0200

    Add project files.

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sumedStudyGroup.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult ListPeople()
+        {
+            List<Models.MyClass> people = new List<Models.MyClass>();
+
+            people.Add(new Models.MyClass { StudentNumber = "u23788918", Name = "Sumed", Surname = "Alekar", EmailAddress = "sumedalekar@gmail.com" });
+            people.Add(new Models.MyClass { StudentNumber = "u23624521", Name = "Marcus", Surname = "Rivera", EmailAddress = "marcusrivera@gmail.com" });
+            people.Add(new Models.MyClass { StudentNumber = "u24597729", Name = "Sophia", Surname = "Mitchell", EmailAddress = "sophiamitchell@gmail.com" });
+            people.Add(new Models.MyClass { StudentNumber = "u24116514", Name = "Ethan", Surname = "Harris", EmailAddress = "ethanharris@gmail.com" });
+            people.Add(new Models.MyClass { StudentNumber = "u23010523", Name = "Lily", Surname = "Parker", EmailAddress = "lilyparker@gmail.com" });
+
+            return View(people);
+        }
+    }
+}
---
+++ Models/MyClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace sumedStudyGroup.Models
+{
+	public class MyClass
+	{
+        [Display(Name = "Student Number")]
+        public string StudentNumber { get; set; } 
+
+        [Display(Name ="Name")]
+        public string Name { get; set; }
+
+        [Display(Name ="Surname")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+    }
+}
